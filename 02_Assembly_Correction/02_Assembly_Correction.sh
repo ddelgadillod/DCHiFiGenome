439093c4b21b6309fa9ddca765d2b459e21a5fc1
ddelgadillod <50838265+ddelgadillod@users.noreply.github.com>
2021-10-04 15:16:23 -0500

    Update 02_Assembly_Correction.sh
    path edit

--- 02_Assembly_Correction/02_Assembly_Correction.sh
+++ 02_Assembly_Correction/02_Assembly_Correction.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-ln -s /home/ddelgadillo/DC-Assembly/01_DC_Canu_Assembly/HiCanuAsm/canu_asm.cntgs_v1.fasta
+
+HICDATAPATH=/home/ddelgadillo/DC-Assembly/Data/HiC
+ASMPATH=/home/ddelgadillo/DC-Assembly/01_DC_Canu_Assembly/HiCanuAsm
+
+ln -s $ASMPATH/canu_asm.cntgs_v1.fasta
 draft=canu_asm.cntgs_v1.fasta
 
 bwa index -a bwtsw canu_asm.cntgs_v1.fasta  
@@ -8,8 +12,8 @@ bwa index -a bwtsw canu_asm.cntgs_v1.fasta
 #Canu draft assembly v2 corrected with raw HiC reads
 
 
-HiC1=/home/ddelgadillo/DC-Assembly/Data/HiC/barrero-meneses-diacol-capiro_S3HiC_R1.fastq.gz
-HiC2=/home/ddelgadillo/DC-Assembly/Data/HiC/barrero-meneses-diacol-capiro_S3HiC_R2.fastq.gz
+HiC1=$HICDATAPATH/barrero-meneses-diacol-capiro_S3HiC_R1.fastq.gz
+HiC2=$HICDATAPATH/barrero-meneses-diacol-capiro_S3HiC_R2.fastq.gz
 
 bwa mem canu_asm.cntgs_v1.fasta $HiC1 $HiC2 -t 20 -k 20 > alCanuv1PEHiC.sam
 
@@ -17,18 +21,22 @@ samtools sort alCanuv1PEHiC.sam -@ 20 -o alCanuv1PEHiC.bam
 samtools index -@ 20 alCanuv1PEHiC.bam
 ALLHiC_corrector -m alCanuv1PEHiC.bam -r canu_asm.cntgs_v1.fasta -o canu_asm.cntgs_v1_corrv2.fasta -t 20
 
-cp canu_asm.cntgs_v1_corrv2.fasta /home/ddelgadillo/DC-Assembly/01_DC_Canu_Assembly/HiCanuAsm/
+cp canu_asm.cntgs_v1_corrv2.fasta $ASMPATH
+
+
+#HiC 
+
 
 #Canu draft assembly v3 corrected with HiC valid contacts
 
-HiC1=/home/ddelgadillo/DC-Assembly/Data/HiC/bm_pot_R1.fastq
-HiC2=/home/ddelgadillo/DC-Assembly/Data/HiC/bm_pot_R1.fastq
+HiC1Valid=$HICDATAPATH/bm_pot_R1.fastq
+HiC2Valid=$HICDATAPATH/bm_pot_R1.fastq
 
 
-bwa mem canu_asm.cntgs_v1.fasta $HiC1 $HiC2 -t 20 -k 20 > alCanuv1PEHiCValid.sam
+bwa mem canu_asm.cntgs_v1.fasta $HiC1Valid $HiC2Valid -t 20 -k 20 > alCanuv1PEHiCValid.sam
 
 samtools sort alCanuv1PEHiCValid.sam -@ 20 -o alCanuv1PEHiCValid.bam
 samtools index -@ 20 alCanuv1PEHiCValid.bam
 ALLHiC_corrector -m alCanuv1PEHiCValid.bam -r canu_asm.cntgs_v1.fasta -o canu_asm.cntgs_v1_corrv3.fasta -t 20
 
-cp canu_asm.cntgs_v1_corrv3.fasta /home/ddelgadillo/DC-Assembly/01_DC_Canu_Assembly/HiCanuAsm/
+cp canu_asm.cntgs_v1_corrv3.fasta $ASMPATH
